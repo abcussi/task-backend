5918d098c9f8bb0acd22647e6f6bf9b54bea55c4
Angel C <angel.cussi@vizonomy.com>
2024-05-05 05:16:44 -0400

    style the code add comments and order

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const taskController = require('../controller/taskController');
-const { JwtAuthenticate } = require('../middleware/authMiddleware');
+const taskController = require("../controller/taskController");
+const { JwtAuthenticate } = require("../middleware/authMiddleware");
 const { validateTask } = require("../middleware/validationMiddleware");
 
-router.post('/', [JwtAuthenticate, validateTask],taskController.createTask);
-router.get('/:userId', [JwtAuthenticate],taskController.getTaskByUserId);
-router.put('/:taskId', [JwtAuthenticate, validateTask],taskController.updateById);
-router.delete('/:taskId', [JwtAuthenticate],taskController.deleteTask);
-router.get('/', [JwtAuthenticate],taskController.getAllTasks);
+// Apply JWT Authentication Middleware
+router.use(JwtAuthenticate);
+
+router.post("/", validateTask, taskController.createTask);
+router.put("/:taskId", validateTask, taskController.updateById);
+router.delete("/:taskId", taskController.deleteTask);
+router.get("/:userId", taskController.getTaskByUserId);
+router.get("/", taskController.getAllTasks);
 
 module.exports = router;

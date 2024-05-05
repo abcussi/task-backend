5ab20652afe5895fc951fb6859090f95608f331e
Angel C <angel.cussi@vizonomy.com>
2024-05-04 23:40:54 -0400

    add enpoint to user healtch check and init mongo

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const app = express();
-const apiRouter = require('./routes/api'); // Ajusta la ruta
+const apiRouter = require('./routes/api');
+const users = require('./routes/users');
+const healthCheck = require("./routes/healthCheck");  
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const { initializeMongooseConnection } = require('./db/connection');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.use('/apiv1', apiRouter);
+app.use('/users',users);
+app.use('/',healthCheck);
+initializeMongooseConnection();
+
+
 const port = 3000;
-
-app.get("/health-check", function (req, res) {
-  function formatDate(date) {
-    const year = date.getFullYear();
-    const month = date.toLocaleString('en-US', { month: 'short' });
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}/${month}/${day}`;
-  }
-  const currentDate = new Date();
-  res.send(formatDate(currentDate)); // Send the formatted date
-});
-
 const server = app.listen(port, function () {
   console.log("Server listening on port " + port);
 });

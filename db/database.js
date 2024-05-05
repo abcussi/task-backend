3ba39ad799627d26986212310cf314b6817baf12
Angel C <angel.cussi@vizonomy.com>
2024-05-05 17:02:38 -0400

    change vars on env

--- db/database.js
+++ db/database.js
@@ -3,8 +3,8 @@ require('dotenv').config(); // Load environment variables
 const dbConfig = {
   username: encodeURIComponent(process.env.MONGO_INITDB_ROOT_USERNAME),
   password: encodeURIComponent(process.env.MONGO_INITDB_ROOT_PASSWORD),
-  host: process.env.MONGO_HOST || 'localhost',
-  port: process.env.MONGO_PORT || 27017,
+  host: process.env.MONGO_INITDB_HOST || 'localhost',
+  port: process.env.MONGO_INITDB_PORT || 27017,
   database: process.env.MONGO_INITDB_DATABASE || '', // Set the database name here if required
   options: {
     autoIndex: false,

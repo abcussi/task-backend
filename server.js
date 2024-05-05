974903ed676199144e72ab8840aebeed2f106e09
Angel C <angel.cussi@vizonomy.com>
2024-05-04 21:57:40 -0400

    add health check and add apiv1 calls to tasks

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const apiRouter = require('./routes/api'); // Ajusta la ruta
 
-
-
-app.get('/', function(req, res){
-    res.json("Hello World");
-   });
-
+app.use('/apiv1', apiRouter);
 const port = 3000;
 
+app.get("/health-check", function (req, res) {
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = date.toLocaleString('en-US', { month: 'short' });
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}/${month}/${day}`;
+  }
+  const currentDate = new Date();
+  res.send(formatDate(currentDate)); // Send the formatted date
+});
+
 const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+  console.log("Server listening on port " + port);
 });

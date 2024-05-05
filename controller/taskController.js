3ea35e8af54fa65a24b30f8633de42eaf181847a
Angel C <angel.cussi@vizonomy.com>
2024-05-05 02:42:46 -0400

    add get all

--- controller/taskController.js
+++ controller/taskController.js
@@ -19,6 +19,18 @@ const createTask = async (req, res, next) => {
   }
 };
 
+const getAllTasks = async (req, res, next) => {
+  try {
+    const tasks = await taskService.getAllTasks();
+    if (!tasks.length) {
+      return res.status(404).json({ status: 'error', message: 'No tasks found' });
+    }
+    return res.json({ status: 'success', message: 'Tasks found', data: tasks });
+  } catch (error) {
+    handleError(res, error);
+  }
+}
+
 const getTaskByUserId = async (req, res, next) => {
   const { userId } = req.params;
   if (!userId) {
@@ -75,4 +87,5 @@ module.exports = {
   getTaskByUserId,
   updateById,
   deleteTask,
+  getAllTasks
 };

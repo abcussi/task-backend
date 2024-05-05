61598fe53331d4ad24e238715eb5950ef1a8d224
Angel C <angel.cussi@vizonomy.com>
2024-05-05 02:43:18 -0400

    fix name

--- routes/healthCheckRoute.js
+++ routes/healthCheckRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const checkController = require("../controller/healthCheckController");
-const { JwtAuthenticate } = require("../middleware/authMiddelware");
+const { JwtAuthenticate } = require("../middleware/authMiddleware");
 
 router.get("/health-check", [JwtAuthenticate], checkController.check);
 

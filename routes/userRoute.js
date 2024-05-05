6044b0b79283370e42a8efac0f9bb915e74124b9
Angel C <angel.cussi@vizonomy.com>
2024-05-05 02:43:26 -0400

    fix name

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controller/userController");
-const { validateUser } = require("../middleware/validationMiddelware");
+const { validateUser } = require("../middleware/validationMiddleware");
 
 router.post("/login", [validateUser], userController.authenticate);
 router.post('/signup', [validateUser], userController.signup);

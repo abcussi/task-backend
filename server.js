16958fc889231aaacaaced1954cd66d5b5522427
Angel C <angel.cussi@vizonomy.com>
2024-05-05 05:17:09 -0400

    add config for packages

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require("express");
 const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const { initializeMongooseConnection } = require('./db/connection');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const csurf = require('csurf');
+const {
+  addCsrfToken
+} = require('./middleware/csrfMiddleware'); // Adjust path accordingly
+
 //global middlewares
 const limiter = require("./middleware/limiterMiddleware");
 
-//routes
-const healthCheck = require("./routes/healthCheckRoute");  
-const users = require('./routes/userRoute');
-const tasks = require('./routes/taskRoute');
 
 //configurations and middlewares for the app
 app.use(logger('dev'));
@@ -18,13 +19,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(limiter);
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "http://localhost:3003"],
+    styleSrc: ["'self'", "http://localhost:3003"]
+  }
+}));
+
+//db connection import and initialization
+const { initializeMongooseConnection } = require('./db/connection');
+
+// Initialize `csurf` middleware
+const csrfProtection = csurf({ cookie: true });
+
+// Apply CSRF protection to all routes
+app.use(csrfProtection);
+app.use(addCsrfToken);
+
+//routes
+const healthCheck = require("./routes/healthCheckRoute");  
+const users = require('./routes/userRoute');
+const tasks = require('./routes/taskRoute');
 
 //db connections
 initializeMongooseConnection();
 
 //routes
-app.use('/users',users);
 app.use('/',healthCheck);
+app.use('/users',users);
 app.use('/tasks', tasks);
 
 //server listening

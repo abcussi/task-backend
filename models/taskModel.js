a0527e81c4321435fc267bfff2dde317ee768067
Angel C <angel.cussi@vizonomy.com>
2024-05-05 03:12:04 -0400

    fix model and delete validation from models

--- models/taskModel.js
+++ models/taskModel.js
@@ -2,17 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const TaskSchema = new Schema(
   {
-    userid: {
+    userId: {
       type: String,
       required: true,
     },
     title: {
       type: String,
-      required: true,
     },
     description: {
       type: String,
-      required: true,
     },
     status: {
       type: String,

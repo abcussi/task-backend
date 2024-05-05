d6e2a2f0480137884e531b5d0f70b9199eb34a8b
Angel C <angel.cussi@vizonomy.com>
2024-05-05 00:38:06 -0400

    add missing vars to the env file

--- db/database.js
+++ db/database.js
@@ -3,9 +3,9 @@ require('dotenv').config(); // Load environment variables
 const dbConfig = {
   username: encodeURIComponent(process.env.MONGO_INITDB_ROOT_USERNAME),
   password: encodeURIComponent(process.env.MONGO_INITDB_ROOT_PASSWORD),
-  host: 'localhost',
-  port: 27017,
-  database: '', // Set the database name here if required
+  host: process.env.MONGO_HOST || 'localhost',
+  port: process.env.MONGO_PORT || 27017,
+  database: process.env.MONGO_INITDB_DATABASE || '', // Set the database name here if required
   options: {
     autoIndex: false,
     authSource: 'admin'

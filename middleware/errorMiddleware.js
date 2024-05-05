cb0b31af6416f3d9478feb49a3eb56025b502bbd
Angel C <angel.cussi@vizonomy.com>
2024-05-05 01:47:25 -0400

    set error handler

---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,11 @@
+const errorMiddleware = (err, req, res, next) => {
+    console.error(err.stack);
+    
+    if (err.name === 'ValidationError') {
+      return res.status(400).json({ status: 'error', message: err.message });
+    }
+    
+    res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+  };
+  
+  module.exports = errorMiddleware;
